d9d229adc0aa5e14479aa774254c8918d7e8f21c
jexline <joeyexline@gmail.com>
2023-02-23 11:31:02 -0800

    Add files via upload

---
+++ src/main/java/com/exline/craftsaddles/CraftSaddlesMain.java
@@ -0,0 +1,12 @@
+package com.exline.craftsaddles;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CraftSaddlesMain implements ModInitializer {
+
+    public static final String MOD_ID = "craftsaddles";
+
+    @Override
+    public void onInitialize() {
+    }
+}
